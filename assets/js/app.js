50d37bad8f7a3b5a0971a63d23cbb8ee0f017556
Nadia Cuadros <november138@hotmail.com>
2017-03-09 18:44:14 -0500

    terminando js del pfs2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-var coder = prompt("¡Bienvenida a LABORATORIA!\n¿Cual es tu nombre?");
+window.onload=function(){prompt("¡Bienvenida a LABORATORIA!\n¿Cual es tu nombre?");}
+
 function myFunction() {
 	document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -25,3 +26,14 @@ var mostarOcultar = function(divtexte) {
 		divtext.style.display = "block";
 	}
 }
+
+function  (){
+	var correctas = 0;
+	var uno = document.getElementsByName('quizz1');
+	for(var i=0; i<uno.length;i++){
+		if(uno[0]==)//la idea es sumar uno a correctas si al marcar el test coincide con la correcta
+	}
+}
+
+
+quizz1, quizz2, quizz3, test1, test2, test3 son arrays
